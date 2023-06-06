bbb27743ae3d72c97d216c04e3de0c059e160ca0
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-06-06 15:43:55 +0100

    task 11

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,16 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    i = 1
-    pow = 1
-    if b > 0:
-        while i <= b:
-            pow = pow*a
-            i += 1
-    elif b == 0:
-        pow = 1
-    else:
-        b = b * -1
-        while i <= b:
-            pow = pow/a
-            i += 1
-    return pow
+    return a ** b
