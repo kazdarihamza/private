324d6abb3971a795e30b4d51ac866cfbd7eadd30
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-06-06 15:08:20 +0100

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,2 +1,7 @@
-i = 'a'
-print(ord('a'))
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        if ord(c) > 96 and ord(c) < 123:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end='')
+    print("")
