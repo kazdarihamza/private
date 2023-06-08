2ba8f04f70e412bbd9345b07dd1de7aaa51ad046
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-06-08 12:21:59 +0100

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-import sys
-n = 2
-if len(sys.argv) == 1:
-    print("0 arguments.")
-elif len(sys.argv) == 2:
-    print("1 argument:")
-    print(f"1: {sys.argv[1]}")
-else:
-    print(f"{len(sys.argv)} arguments")
-    while n <= len(sys.argv): 
-        print(f"{n-1}: {sys.argv[n-1]}")
-        n += 1
+if __name__ == "__main__":
+    import sys
+    n = 2
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("1 argument:")
+        print(f"1: {sys.argv[1]}")
+    else:
+        print(f"{len(sys.argv)} arguments")
+        while n <= len(sys.argv): 
+            print(f"{n-1}: {sys.argv[n-1]}")
+            n += 1
