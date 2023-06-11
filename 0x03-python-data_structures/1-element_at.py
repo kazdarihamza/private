e792d2f91767e9831196aa3a03aec233f218d1a1
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-06-11 13:49:10 +0100

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     for i in range(len(my_list)):
-        if idx > len(my_list) or idx < 0:
+        if idx > (len(my_list) - 1) or idx < 0:
             return None
 
     return my_list[idx]
