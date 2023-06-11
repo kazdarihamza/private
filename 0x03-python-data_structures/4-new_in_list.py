cfa7bc2a8ac50c62a6ab5d51a4ca8e42be117476
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-06-11 13:54:29 +0100

    task 6

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,7 @@ def new_in_list(my_list, idx, element):
     for i in range(len(my_list)):
         if idx > (len(my_list) - 1) or idx < 0:
             return my_list
+
     new_list = [x for x in my_list]
     new_list[idx] = element
     return new_list
