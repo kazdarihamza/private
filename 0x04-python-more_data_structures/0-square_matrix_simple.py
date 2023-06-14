6f0dc8cb2cc9f60de0062903bf56224c778e84ce
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-06-14 15:41:37 +0100

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,4 +2,4 @@
 def square_matrix_simple(matrix=[]):
     if not matrix:
         return None
-    return list(list(map(lambda a: a*a, numbers)) for  numbers in matrix)
+    return list(list(map(lambda a: a*a, numbers)) for numbers in matrix)
